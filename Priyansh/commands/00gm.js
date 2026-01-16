341e94c4d0afc05575383d842c6bedee84a6ba23
dholsk31-prog <dholsk31@gmail.com>
2026-01-16 07:49:44 +0530

    Add files via upload

---
+++ Priyansh/commands/00gm.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "gm",
+    version: "2.1.1",
+  hasPermssion: 0,
+  credits: "PRINCE RAJPUT", 
+  description: "Just Respond",
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils, client, global }) => {
+  var name = await Users.getNameUser(event.senderID);
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("gm") ||
+     react.includes("Gm") ||
+     react.includes("Morning") ||
+react.includes("morning")) {
+    var msg = {
+        body: `
+ ╔══❉্᭄͜͡💕❉্᭄͜͡💕❉্᭄͜͡════╗
+❉্᭄͜͡💕      Good Morning     💕❉্᭄͜͡      
+   🅑🅔🅐🅤🅣🅘🅕🅤🅛 
+🌸       🅜🅞🅡🅝🅘🅝🅖🤩       
+ ╚══❉্᭄͜͡💕❉্᭄͜͡💕❉্᭄͜͡════╝
+.          💕 Uth jao gays💓
+.•°``°•.¸.•°``°•.
+(  Morning  )  ☔ ❄
+ `•.¸  💓   ¸.•`  🌹 🌹 💚
+     ° •.¸¸.•° Good 
+                          Sweet Morning 🍬💓🍬💓`,attachment: fs.createReadStream(__dirname + `/noprefix/gmm.mp4`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😈", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
+
+  }
